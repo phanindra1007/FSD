f378cdfc7d7037196060ef41f763fa8be4d528f0
phanindra1007 <2420030553@klh.edu.in>
2026-01-06 08:26:48 +0530

    Add files via upload

---
+++ HibernateProductCRUD/src/main/java/com/klu/HibernateUtil.java
@@ -0,0 +1,24 @@
+package com.klu;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Product.class)
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
